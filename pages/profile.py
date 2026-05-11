aec41e5e08cf49bbca346ddb4d88f4940201e40d
sakshirani1228 <sakshirani1228@gmail.com>
2026-05-11 21:06:51 +0530

    Update profile.py

--- pages/profile.py
+++ pages/profile.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 import pymongo
-conn=pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.2")
+conn=pymongo.MongoClient("mongodb+srv://sakshirani1228_db_user:7bhnzfdX2Yuq6ZUK@cluster0.qkhw1qi.mongodb.net/?appName=Cluster0")
 mydb=conn["cv"]
 my=mydb["user_info"]
 with st.spinner("Loading..."):
