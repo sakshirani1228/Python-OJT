c91c002dc742399dd1b3321a3f79d91456b3fb2a
sakshirani1228 <sakshirani1228@gmail.com>
2026-05-11 21:06:00 +0530

    Update Signup.py

--- pages/Signup.py
+++ pages/Signup.py
@@ -29,7 +29,7 @@ def get_data():
     st.write(dob)
     st.write(co)
     st.write(str1)
-    conn=pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.2")
+    conn=pymongo.MongoClient("mongodb+srv://sakshirani1228_db_user:7bhnzfdX2Yuq6ZUK@cluster0.qkhw1qi.mongodb.net/?appName=Cluster0")
     mydb=conn["cv"]
     my=mydb["user_info"]
     my.insert_one({"username":name,"password":Password,"course":c,"gender":g,"address":address,"dob":str(dob),"color":co,"photo":str1})
